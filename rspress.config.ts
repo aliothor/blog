e467bb43aeec9b54f1d0a0acc150ef4e9b12dffb
Tao Jianhang <1516544795@qq.com>
2023-10-11 10:40:35 +0800

    init(repo): 初始化仓库

---
+++ rspress.config.ts
@@ -0,0 +1,26 @@
+import * as path from 'path'
+import { defineConfig } from 'rspress/config'
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'Aliothor',
+  description: 'Aliothor Blog',
+  icon: '/rspress-icon.png',
+  logo: {
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
+  },
+  themeConfig: {
+    socialLinks: [{ icon: 'github', mode: 'link', content: 'https://github.com/aliothor/blog' }],
+  },
+  markdown: {
+    mdxRs: true,
+  },
+  builderConfig: {
+    output: {
+      distPath: {
+
+      },
+    },
+  },
+})
---
+++ src/components/CustomComponent.tsx
@@ -0,0 +1,5 @@
+export default function App() {
+    return (
+        <h1>组件测试11</h1>
+    )
+}
